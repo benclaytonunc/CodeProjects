2455cc78bfe5e758b740d33ac9fb43c25da28afb
benclaytonunc <55894689+benclaytonunc@users.noreply.github.com>
2020-10-16 14:02:45 -0400

    jeff

---
+++ src/main/java/com/comp301/a06image/ZoomDecorator.java
@@ -0,0 +1,45 @@
+package com.comp301.a06image;
+
+import java.awt.*;
+
+public class ZoomDecorator implements Image {
+    private Image im;
+    private int multiple;
+    public ZoomDecorator(Image image, int multiplier) {
+        this.im = image;
+        this.multiple = multiplier;
+        if (multiple < 0) {
+            throw new IllegalArgumentException();
+        }
+    }
+    public ZoomDecorator(Image image) {
+        this.im = image;
+        this.multiple = 2;
+    }
+
+    @Override
+    public Color getPixelColor(int x, int y) {
+        if (x < 0 || y < 0) {
+            throw new IllegalArgumentException();
+        }
+        if (x >= getWidth() || y >= getHeight()) {
+            throw new IllegalArgumentException();
+        }
+        return im.getPixelColor(x,y);
+    }
+
+    @Override
+    public int getWidth() {
+        return getWidth()*multiple;
+    }
+
+    @Override
+    public int getHeight() {
+        return getHeight()*multiple;
+    }
+
+    @Override
+    public int getNumLayers() {
+        return 1+im.getNumLayers();
+    }
+}

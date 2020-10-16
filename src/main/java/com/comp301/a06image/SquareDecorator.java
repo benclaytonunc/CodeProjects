ed3498f3c1bb25de3090b032008381eaec6e7f45
benclaytonunc <55894689+benclaytonunc@users.noreply.github.com>
2020-10-16 13:32:36 -0400

    jeff

--- src/main/java/com/comp301/a06image/SquareDecorator.java
+++ src/main/java/com/comp301/a06image/SquareDecorator.java
@@ -34,10 +34,10 @@ public class SquareDecorator implements Image{
         if (y >= sqY + sqSize) {
             return im.getPixelColor(x,y);
         }
-        if (x <= sqX) {
+        if (x < sqX) {
             return im.getPixelColor(x,y);
         }
-        if (y <= sqY) {
+        if (y < sqY) {
             return im.getPixelColor(x,y);
         }
         return sqColor;
